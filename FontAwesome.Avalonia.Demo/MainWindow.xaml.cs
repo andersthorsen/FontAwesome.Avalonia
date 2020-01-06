bc645c9912ea1317b69e2cb1c55505d994770dd3
Anders Christian Thorsen <anders@anders.to>
2020-01-06 09:59:22 +0100

    Add a demo project to be able to test faster.

---
+++ FontAwesome.Avalonia.Demo/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FontAwesome.Avalonia.Demo
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+
+            this.FindControl<TextBlock>("TestTextBlock").Text = char.ConvertFromUtf32(0xf0e0);
+        }
+    }
+}
---
+++ FontAwesome.Avalonia.Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Logging;
+using Avalonia.Logging.Serilog;
+
+namespace FontAwesome.Avalonia.Demo
+{
+    class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args)
+        {
+            var assembly = Assembly.GetAssembly(typeof(IRotatable));
+
+            foreach (var resourceName in assembly.GetManifestResourceNames())
+                System.Diagnostics.Debug.WriteLine(resourceName);
+
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug(LogEventLevel.Verbose);
+    }
+}
